7f1273dc1520e33066c7da9a8294682f70e4b834
Bernard Delhez <bernarddelhez@gmail.com>
2015-11-04 15:11:44 +0100

    first working version

---
+++ README.md
@@ -0,0 +1,3 @@
+#Sale Order Improvements. 
+
+This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions.
---
+++ __openerp__.py
@@ -0,0 +1,22 @@
+﻿{
+    'name': 'Sale Order Improvements',
+    'version': '1.0',
+    'category': 'Sales',
+    'description': 
+    """
+    Sale Order Improvements. 
+    
+    This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions.
+    """,
+    'depends': [
+        'base_setup',
+        'sale',
+    ],
+    'data': [
+        'report/report_saleorder.xml',
+        'report/report_saleorder_conditions.xml',
+    ],
+    'installable': True,
+    'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+}
